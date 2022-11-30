abfc93d203bc3b8cd6410645f8c4b85c311c520d
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-11-30 17:15:04 +0700

    feat: done

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 > Deploy ดีมั้ย?
 
 > ไม่ดี ขอบคุณครับ
+
+Use [Deploy Deemai](https://github.com/narze/deploydeemai.today)'s api 🙇
+
+## Demo & Example
+
+[Action File](.github/workflows/deploydeemai.yml)
+
+See pull request tabs of this repo :trollface:
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,56 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+import { fetch } from "undici";
+
+const DeployDeemaiApi = "https://deploydeemai.today/api";
+
+interface DeployDeemaiResponse {
+  message: string;
+  deploydeemai: boolean;
+}
+
+async function deployDeemai(): Promise<DeployDeemaiResponse> {
+  const response = (await fetch(DeployDeemaiApi).then((r) =>
+    r.json()
+  )) as DeployDeemaiResponse;
+
+  if (
+    "message" in response &&
+    "deploydeemai" in response &&
+    typeof response.message === "string" &&
+    typeof response.deploydeemai === "boolean"
+  ) {
+    return response;
+  } else {
+    return {
+      message: "ขนาด GitHub Actions นี้ยังทำงานผิดพลาด อย่า deploy เถอะ",
+      deploydeemai: false,
+    };
+  }
+}
 
 async function main() {
-  // todo
+  const token = core.getInput("token");
+  const octokit = github.getOctokit(token);
+
+  const { message, deploydeemai } = await deployDeemai();
+
+  const { pull_request } = github.context.payload;
+
+  if (!pull_request) {
+    throw new Error("Pull Request not found");
+  }
+
+  await octokit.rest.pulls.createReviewComment({
+    ...github.context.repo,
+    pull_number: pull_request.number,
+    body: `### ${deploydeemai ? "✅" : "❌"} ${message}`,
+  });
+
+  if (!deploydeemai) {
+    core.setFailed("Please avoid deploying");
+  }
 }
 
 main().catch((err) => {
